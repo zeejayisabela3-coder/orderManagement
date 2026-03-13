c39af28a9881a57dbde687e99e6248192b45a640
zeejayisabela3-coder <zeejayisabela3@gmail.com>
2026-03-13 13:39:20 +0800

    new

---
+++ orderManagementModels/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderManagementModels
+{
+    public class Order
+    {
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
