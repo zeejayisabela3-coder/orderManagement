c27393916cc72c19d15542dfa306a07e9c80f1a5
zeejayisabela3-coder <zeejayisabela3@gmail.com>
2026-03-13 12:55:21 +0800

    new having models, business and data logic layer

--- orderManagementModels/Order.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OrderManagementModels
-{
-    public class Order
-    {
-        public string ItemName { get; set; }
-        public int Quantity { get; set; }
-        public double TotalPrice { get; set; }
-    }
-}
